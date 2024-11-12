dcfcb138864934e162813a12f8d49982f3612ceb
quanling-hub <1203499185@qq.com>
2024-11-12 14:42:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 wget http://116.204.100.57/metabobank/outmongo.tar.gz
-wget http://116.204.100.57/metabobank/nsmetabobank.tar.gz
-wget http://116.204.100.57/metabobank/mongod.tar.gz
-wget http://116.204.100.57/metabobank/omics.tar.gz
+wget http://116.204.100.57/metabobank/nsmetabobank_v2.0.tar.gz
+wget http://116.204.100.57/metabobank/mongod_v3.6.tar.gz
+wget http://116.204.100.57/metabobank/omics_v2.0.tar.gz
 
 tar -xzvf data1.tar.gz -C /
+
 chmod -R 777 /data1
+
+cp docker-compose.yml /data1
 
 tar -xzvf outmongo.tar.gz -C /
 
-docker load -i nsmetabobank.tar.gz
+docker load -i nsmetabobank_v2.0.tar.gz
 
-docker load -i mongod.tar.gz
+docker load -i mongod_v3.6.tar.gz
 
-docker load -i omics.tar.gz
+docker load -i omics_v2.0.tar.gz
 
 docker-compose up -d
 
-echo "Now you can use your metabobank in $(hostname -I | awk '{print $1}'):8085 or 127.0.0.1:8085"
+echo "Now you can use your metabobank in $(hostname -I | awk '{print $1}'):8085 or 127.0.0.1:8085" 
